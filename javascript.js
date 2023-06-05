31fb5d676d45996d902ac67829f9496fb69ff8f5
dalshek <cdiezpag@gmail.com>
2023-06-06 00:34:35 +0200

    Adding second class and event

--- javascript.js
+++ javascript.js
@@ -1,6 +1,5 @@
 //-----GLOBAL VARIABLES----------
 let div='';
-
 //---------------CREATIN THE MAIN DIV------------
 const divMain = document.createElement('div');
 divMain.setAttribute("class", "main")
@@ -9,19 +8,21 @@ document.body.appendChild(divMain);
 //Creating the 16x16 grid
 const mainDom = document.querySelector('.main');
 
-
-
 let createDivs = () => {
     div = document.createElement('div');
     div.setAttribute("class", "grid");
+    mainDom.addEventListener("mouseover", () =>{
+        div.classList.add('hover');
+    })
     mainDom.appendChild(div);
-
 }
 
 for(let i=1; i<=16;i++){
     
     for(let j=1; j<=16; j++){
         createDivs();
-    }   
-    
+    }      
 }
+
+//HOVER EFFECT
+
